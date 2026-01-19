511872f57b7b70d51be6a55cee8481825ff75be6
ultramunnn <munir63577@student.ub.ac.id>
2026-01-19 17:18:52 +0700

    update cv

--- src/components/AppProfile.vue
+++ src/components/AppProfile.vue
@@ -22,7 +22,7 @@
         </p>
         <a
           class="mt-4 inline-flex items-center rounded-md border border-accent px-3 py-2 text-xs text-accent  transition-colors hover:bg-accent hover:text-paper dark:text-paper dark:hover:bg-accent dark:hover:text-paper"
-          href="https://1drv.ms/w/c/8bdbbea29cc945c2/IQCZf9jGs88NRZUQ1Faqjmn7AfPFc-dfzvsnfkpMEecrusY?e=hcPmOc"
+          href="https://1drv.ms/w/c/8bdbbea29cc945c2/IQBCxwvvHxzfQ4-bzBOVEgO8AfABg3TBBhX7Jxs7KlNZkE4?e=7QbJ0s"
           target="_blank"
           rel="noreferrer"
         >
